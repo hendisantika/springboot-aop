d8a92e69a65fa5ea529f0e9ca14b9a9906c7b072
Hendi Santika <hendisantika@gmail.com>
2021-01-27 06:37:21 +0700

    #4 - Add Employee Service

---
+++ src/main/java/com/hendisantika/springbootaop/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootaop.service;
+
+import com.hendisantika.springbootaop.model.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-aop
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/01/21
+ * Time: 06.36
+ */
+@Component
+public class EmployeeService {
+    private static final List<Employee> employeeLst = new ArrayList<>();
+
+}
