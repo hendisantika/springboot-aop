d8a741fa8cecc68ecdb4ad4bcf2ace4b0abc90d3
Hendi Santika <hendisantika@gmail.com>
2021-01-27 06:41:08 +0700

    #8 - Add calculate in LoggerAspect

--- src/main/java/com/hendisantika/springbootaop/aop/LoggerAspect.java
+++ src/main/java/com/hendisantika/springbootaop/aop/LoggerAspect.java
@@ -2,12 +2,17 @@ package com.hendisantika.springbootaop.aop;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,6 +35,20 @@ public class LoggerAspect {
     @After("@annotation(MethodLogger)")
     public void afterMethodStart(JoinPoint point) {
         log.info("Method " + point.getSignature().getName() + " Ended at " + LocalDateTime.now());
+    }
+
+    @Around("@annotation(CalculatePerformance)")
+    public void calculate(ProceedingJoinPoint point) {
+        LocalTime startTime = LocalTime.now();
+        try {
+            point.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            LocalTime endTime = LocalTime.now();
+            log.info("Processing time of Method " + point.getSignature().getName() + " -> "
+                    + SECONDS.between(startTime, endTime));
+        }
     }
 
 }
