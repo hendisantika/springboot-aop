475601813bde44484833ac51074866450a729434
Hendi Santika <hendisantika@gmail.com>
2021-01-27 06:38:41 +0700

    #6 - Add LoggerAspect

---
+++ src/main/java/com/hendisantika/springbootaop/aop/LoggerAspect.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootaop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-aop
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/01/21
+ * Time: 06.38
+ */
+@Configuration
+@Aspect
+public class LoggerAspect {
+}
