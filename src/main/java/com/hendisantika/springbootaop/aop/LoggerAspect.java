c0f8fecb0022d20e946b4801c9140f7c43e5132c
Hendi Santika <hendisantika@gmail.com>
2021-01-27 06:40:33 +0700

    #7 - Add afterMethodStart in LoggerAspect

--- src/main/java/com/hendisantika/springbootaop/aop/LoggerAspect.java
+++ src/main/java/com/hendisantika/springbootaop/aop/LoggerAspect.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootaop.aop;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
@@ -24,7 +25,11 @@ public class LoggerAspect {
     @Before("@annotation(MethodLogger)")
     public void beforeMethodStart(JoinPoint point) {
         log.info("Method " + point.getSignature().getName() + " Started at " + LocalDateTime.now());
+    }
 
+    @After("@annotation(MethodLogger)")
+    public void afterMethodStart(JoinPoint point) {
+        log.info("Method " + point.getSignature().getName() + " Ended at " + LocalDateTime.now());
     }
 
 }
